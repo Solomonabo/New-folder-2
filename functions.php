f363c050e2718a695a48e63c21dfc7b503e758d8
Solomonabo <46652792+Solomonabo@users.noreply.github.com>
2019-05-12 10:16:55 -0700

    Initial commit

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+  <?php
+
+ $x = 10;
+
+ $y =15;
+
+ $a =$x +$a 
+
+ echo $a //out put 25//
+
+
+   echo str_word_count ("We are close to the house") // out put -count the number of words in the string//
+
+   echo strrev("My name is Solomon ") // out put -reverses the strings//
+
+$sports = ( "soccer, basket ball, football")
+  
+  echo "I like " . "$sports[0]" // out put I like soccer
+
+
+
+?>
+?>
